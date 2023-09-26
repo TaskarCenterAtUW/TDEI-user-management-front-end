25e731aefcee1c47f6080a0b3db9d448a73d7287
shweta2101 <shwethap@vindago.in>
2023-09-26 17:50:43 +0530

    Removed unnecessary code

--- tdei-ui/src/utils/helper.js
+++ tdei-ui/src/utils/helper.js
@@ -4,25 +4,3 @@ export const getUserName = (user, isCurrentUser) => {
   }
   return `${user.first_name} ${user.last_name} ${isCurrentUser ? "(You)" : ""}`;
 };
-
-// export const syntaxHighlight = (json: ) => {
-//   if (typeof json !== 'string') {
-//     json = JSON.stringify(json, null, 2);
-//   }
-//   json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-//   return json.replace(/('(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\'])*'(\\s*:)?)/g, (match: any) => {
-//     let cls = 'string';
-//     if (/^\d+$/.test(match)) {
-//       cls = 'number';
-//     } else if (/^true|false$/.test(match)) {
-//       cls = 'boolean';
-//     } else if (/^null$/.test(match)) {
-//       cls = 'null';
-//     } else if (/^'.+'$/.test(match)) {
-//       cls = 'string';
-//     } else if (/^'.+':$/.test(match)) {
-//       cls = 'key';
-//     }
-//     return '<span class="' + cls + '">' + match + '</span>';
-//   });
-//}
