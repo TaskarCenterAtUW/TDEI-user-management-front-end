404081de4fa9fcb2edda030abb447f1fcc0da55b
shweta2101 <shwethap@vindago.in>
2023-11-21 18:21:25 +0530

    Added project id and forgot password features
    1. Added project id as a part of display in :
       - Dashboard screen (for non-admin)
       - List of project groups (for admin)
    2. Added forgot password button which on click display a modal.

---
+++ tdei-ui/src/components/ForgotPassModal/ForgotPassModal.js
@@ -0,0 +1,36 @@
+
+import React, { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+
+const ForgotPassModal = () => {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <div>
+                Forgot Password?{" "}
+                <Link className="tdei-primary-link" onClick={handleShow}>
+                    Click Here
+                </Link>
+            </div>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Forgot Password?</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Please send an email with your username to : admin@tdei.com</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+};
+
+export default ForgotPassModal;
