e59a130ceeeec6f4a72343d056f14be8bc6a92da
shweta2101 <shwethap@vindago.in>
2024-03-22 16:23:33 +0530

    Changed Stepper along with prev and next buttons

--- tdei-ui/src/components/VerticalStepper/VerticalStepper.js
+++ tdei-ui/src/components/VerticalStepper/VerticalStepper.js
@@ -11,6 +11,8 @@ import FolderZipIcon from '@mui/icons-material/FolderZip';
 import DescriptionIcon from '@mui/icons-material/Description';
 import DatasetIcon from '@mui/icons-material/Dataset';
 import serviceUpload from './../../assets/img/service_upload.svg'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import { Icon } from '@mui/material';
 
 export const ServiceIcon = () => (
@@ -34,12 +36,13 @@ const ColorlibStepIconRoot = styled('div')(({ theme, ownerState }) => ({
     width: 45,
     height: 45,
     borderRadius: '50%',
-    border: `1px solid ${ownerState.active ? '#59C3C8' : '#ccc'}`,
+    border: `1px solid ${ownerState.completed || ownerState.active ? '#59C3C8' : '#ccc'}`,
     boxSizing: 'border-box', 
   }));
   
   const InnerCircle = styled('div')(({ theme, ownerState }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? theme.palette.grey[700] : '#ccc',
+    backgroundColor: ownerState.completed || ownerState.active ? '#59C3C8' : (theme.palette.mode === 'dark' ? theme.palette.grey[700] : '#ccc'),
+    zIndex: 1,
     zIndex: 1,
     color: '#fff',
     width: 35, 
@@ -48,9 +51,6 @@ const ColorlibStepIconRoot = styled('div')(({ theme, ownerState }) => ({
     borderRadius: '50%',
     justifyContent: 'center',
     alignItems: 'center',
-    ...(ownerState.active && {
-        backgroundColor: '#59C3C8',
-      }),
   }));
   
   function ColorlibStepIcon({ active, completed, icon, className }) {
@@ -63,8 +63,8 @@ const ColorlibStepIconRoot = styled('div')(({ theme, ownerState }) => ({
   
     return (
       <ColorlibStepIconRoot ownerState={{ completed, active }} className={className}>
-        <OutlineCircle ownerState={{ active }} />
-        <InnerCircle ownerState={{ active }}> {}
+        <OutlineCircle ownerState={{ active, completed }} />
+        <InnerCircle ownerState={{ active, completed }}>
           {icons[String(icon)]}
         </InnerCircle>
       </ColorlibStepIconRoot>
@@ -111,17 +111,22 @@ export default function VerticalStepper() {
   };
 
   const handleNext = () => {
+    const newCompleted = completed;
     const newActiveStep =
-      isLastStep() && !allStepsCompleted()
-        ? // It's the last step, but not all steps have been completed,
-          // find the first step that has been completed
-          steps.findIndex((step, i) => !(i in completed))
+      isLastStep()
+        ?
+        activeStep
         : activeStep + 1;
     setActiveStep(newActiveStep);
+    newCompleted[activeStep] = true;
+    setCompleted(newCompleted);
   };
 
   const handleBack = () => {
+    const newCompleted = { ...completed };
+    delete newCompleted[activeStep];
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    setCompleted(newCompleted);
   };
 
   const handleStep = (step) => () => {
@@ -155,56 +160,27 @@ export default function VerticalStepper() {
         {steps.map((label, index) => (
           <Step key={label} completed={completed[index]}>
              <StepLabel StepIconComponent={ColorlibStepIcon}>{label}</StepLabel>
-            {/* <StepButton color="inherit" onClick={handleStep(index)}>
-              {label}
-            </StepButton> */}
           </Step>
         ))}
       </Stepper>
       <div>
-        {allStepsCompleted() ? (
           <React.Fragment>
-            <Typography sx={{ mt: 2, mb: 1 }}>
-              All steps completed - you&apos;re finished
-            </Typography>
             <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
               <Box sx={{ flex: '1 1 auto' }} />
-              <Button onClick={handleReset}>Reset</Button>
-            </Box>
-          </React.Fragment>
-        ) : (
-          <React.Fragment>
-            <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
-              Step {activeStep + 1}
-            </Typography>
-            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-              <Button
-                color="inherit"
-                disabled={activeStep === 0}
+              {activeStep === 0 ? (
+                <Box></Box>
+              ): (<Button
+                sx={{border:"1px solid #4DA9AD", color:"#4DA9AD", marginRight: 1}}
                 onClick={handleBack}
-                sx={{ mr: 1 }}
+                startIcon = {<ChevronLeftIcon />}
               >
-                Back
+                Prev
+              </Button>)}
+              <Button className="tdei-primary-button" onClick={handleNext} endIcon={ isLastStep() ? <div></div> : <ChevronRightIcon /> } >
+              { isLastStep() ?  'Submit' : 'Next' } 
               </Button>
-              <Box sx={{ flex: '1 1 auto' }} />
-              <Button onClick={handleNext} sx={{ mr: 1 }}>
-                Next
-              </Button>
-              {activeStep !== steps.length &&
-                (completed[activeStep] ? (
-                  <Typography variant="caption" sx={{ display: 'inline-block' }}>
-                    Step {activeStep + 1} already completed
-                  </Typography>
-                ) : (
-                  <Button onClick={handleComplete}>
-                    {completedSteps() === totalSteps() - 1
-                      ? 'Finish'
-                      : 'Complete Step'}
-                  </Button>
-                ))}
             </Box>
           </React.Fragment>
-        )}
       </div>
     </Box>
   );
--- tdei-ui/src/routes/UploadDataset/UploadDataset.js
+++ tdei-ui/src/routes/UploadDataset/UploadDataset.js
@@ -4,9 +4,9 @@ import Container from "../../components/Container/Container";
 import style from "./UploadDataset.module.css"
 import VerticalStepper from "../../components/VerticalStepper/VerticalStepper"
 const steps = [
-  { title: 'Step 1', content: 'Content for step 1' },
-  { title: 'Step 2', content: 'Content for step 2' },
-  { title: 'Step 3', content: 'Content for step 3' },
+  // { title: 'Step 1', content: 'Content for step 1' },
+  // { title: 'Step 2', content: 'Content for step 2' },
+  // { title: 'Step 3', content: 'Content for step 3' },
 ];
 const UploadDataset = () => {
   return (
