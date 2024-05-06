13b050cf3bda726ddd0d5d752ec4f7fddf1713f7
shweta2101 <shwethap@vindago.in>
2024-05-06 18:52:38 +0530

    data details tab init commit

--- tdei-ui/src/components/DatePicker/DatePicker.js
+++ tdei-ui/src/components/DatePicker/DatePicker.js
@@ -4,14 +4,33 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 
-export default function DatePicker() {
+export default function DatePicker({label}) {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DesktopDatePicker defaultValue={dayjs('2022-04-17')} sx={{
+        <DesktopDatePicker  
+        sx={{
             "& .MuiInputBase-input": {
-              height: "8px" 
-            }
-          }} />
+              height: "5px",
+            },
+            '& .MuiOutlinedInput-root': {
+              '& fieldset': {
+                borderColor: '#E0E0E0',
+              },
+              '&:hover fieldset': {
+                borderColor: '#E0E0E0',
+              },
+              '&.Mui-focused fieldset': {
+                borderColor: '#4382F7',
+              },
+            },
+            '& .MuiInputBase-input::placeholder': {
+              color: 'black', 
+              fontFamily: 'Arial',  
+              fontSize: '16px', 
+            },
+          }} 
+          slotProps={{ textField: { placeholder: label} }}
+          />
     </LocalizationProvider>
   );
 }
--- tdei-ui/src/routes/UploadDataset/MetaDataForm/DatasetDetails.js
+++ tdei-ui/src/routes/UploadDataset/MetaDataForm/DatasetDetails.js
@@ -9,6 +9,8 @@ import DatePicker from "../../../components/DatePicker/DatePicker";
 const DatasetDetails = () => {
   const { values, setFieldValue } = useFormikContext();
   const [datasetType, setDatasetType] = React.useState("");
+  const [collectedMethod, setCollectedMethod] = React.useState("");
+  const [dataSource, setDataSource] = React.useState("");
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFieldValue(name, value);
@@ -17,11 +19,30 @@ const DatasetDetails = () => {
   const handleDatasetTypeSelect = (value) => {
     setDatasetType(value.value);
   };
+  const handleCollectedMethodSelect = (value) => {
+    setCollectedMethod(value.value);
+  };
+  const handleDataSourceSelect = (value) => {
+    setDataSource(value.value);
+  };
   const datasetTypeOptions = [
     { value: 'flex', label: 'Flex' },
     { value: 'pathways', label: 'Pathways' },
     { value: 'osw', label: 'Osw' },
   ];
+  const collectedMethodOptions = [
+    { value: 'manual', label: 'Manual' },
+    { value: 'transform', label: 'Transform' },
+    { value: 'generated', label: 'Generated' },
+    { value: 'av', label: 'AV' },
+    { value: 'others', label: 'Others' }
+  ];
+  const dataSourceOptions = [
+    { value: 'TDEI Tool', label: 'TDEI Tool' },
+    { value: 'Third Party', label: 'Third Party' },
+    { value: 'In House', label: 'In House' },
+  ];
+  var link = <a href={'https://geojson.io/'} target="_blank" rel="noreferrer">geojson.io</a>;
   return (
     <div>
       <div className="d-flex align-items-center" style={{ marginTop: '20px' }}>
@@ -45,7 +66,7 @@ const DatasetDetails = () => {
         </Form.Group>
       </div>
       <div className="d-flex align-items-center" style={{ marginTop: '10px' }}>
-      <Form.Group className="col-3" controlId="datasetType" style={{ marginRight: '40px' }}>
+        <Form.Group className="col-4" controlId="datasetType">
           <Form.Label>Dataset Type<span style={{ color: 'red' }}> *</span></Form.Label>
           <div className={style.selectDropdown}>
             <Select
@@ -56,11 +77,11 @@ const DatasetDetails = () => {
               components={{
                 IndicatorSeparator: () => null
               }}
-              styles={{ container: (provided) => ({ ...provided, width: '100%'}) }}
+              styles={{ container: (provided) => ({ ...provided, width: '100%' }) }}
             />
           </div>
         </Form.Group>
-        <Form.Group className="col-4" controlId="collectedBy">
+        <Form.Group className="col-3" controlId="collectedBy" style={{ marginRight: '40px' }}>
           <Form.Label>Collected By</Form.Label>
           <Form.Control
             type="text"
@@ -69,7 +90,113 @@ const DatasetDetails = () => {
           />
           <ErrorMessage name="collectedBy" component="div" />
         </Form.Group>
-        <DatePicker />
+        <Form.Group className="col-4" controlId="collectedBy">
+          <Form.Label>Collection Date</Form.Label>
+          <DatePicker label={"Select Collection Date"} />
+        </Form.Group>
+      </div>
+      <div className="d-flex align-items-center" style={{ marginTop: '10px' }}>
+        <Form.Group className="col-4" controlId="datasetType">
+          <Form.Label>Collected Method</Form.Label>
+          <div className={style.selectDropdown}>
+            <Select
+              placeholder="Select Method"
+              isSearchable={false}
+              onChange={handleCollectedMethodSelect}
+              options={collectedMethodOptions}
+              components={{
+                IndicatorSeparator: () => null
+              }}
+              styles={{ container: (provided) => ({ ...provided, width: '100%' }) }}
+            />
+          </div>
+        </Form.Group>
+        <Form.Group className="col-4" controlId="collectedBy">
+          <Form.Label>Data Source</Form.Label>
+          <div className={style.selectDropdown}>
+            <Select
+              placeholder="Select Source"
+              isSearchable={false}
+              onChange={handleDataSourceSelect}
+              options={dataSourceOptions}
+              components={{
+                IndicatorSeparator: () => null
+              }}
+              styles={{ container: (provided) => ({ ...provided, width: '100%' }) }}
+            />
+          </div>
+        </Form.Group>
+        <Form.Group className="col-4" controlId="datasetVersion">
+          <Form.Label>Schema Version</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Schema Version"
+            name="schemaVersion"
+          />
+        </Form.Group>
+
+      </div>
+      <div className="d-flex align-items-center" style={{ marginTop: '10px' }}>
+        <Form.Group className="col-3" controlId="datasetVersion" style={{ marginRight: '40px' }}>
+          <Form.Label>TDEI Service Id<span style={{ color: 'red' }}> *</span></Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Schema Version"
+            name="schemaVersion"
+          />
+        </Form.Group>
+        <Form.Group className="col-4" controlId="collectedBy">
+          <Form.Label>Valid From</Form.Label>
+          <DatePicker label={"Select Starting Date"} />
+        </Form.Group>
+        <Form.Group className="col-4" controlId="collectedBy">
+          <Form.Label>Valid To</Form.Label>
+          <DatePicker label={"Select Expiry Date"} />
+        </Form.Group>
+      </div>
+      <div style={{ marginTop: '10px' }}>
+        <Form.Label>Dataset Area</Form.Label>
+        <div className="tdei-hint-text">(hint: Create the bounding box using {link} )</div>
+        <div className="jsonContent">
+          <Form.Control
+            as="textarea"
+            type="text"
+            name="polygon"
+            // onChange={handleTextareaChange}
+            // onBlur={handleBlur}
+            rows={10}
+          // value={geoJson}
+          />
+        </div>
+      </div>
+      <div style={{ marginTop: '10px' }}>
+        <Form.Label>Description</Form.Label>
+        <div className="jsonContent">
+          <Form.Control
+            as="textarea"
+            type="text"
+            name="polygon"
+            // onChange={handleTextareaChange}
+            // onBlur={handleBlur}
+            rows={5}
+            placeholder="Enter Description"
+          // value={geoJson}
+          />
+        </div>
+        </div>
+        <div style={{ marginTop: '10px' }}>
+        <Form.Label>Custom Metadata</Form.Label>
+        <div className="jsonContent">
+          <Form.Control
+            as="textarea"
+            type="text"
+            name="polygon"
+            // onChange={handleTextareaChange}
+            // onBlur={handleBlur}
+            rows={5}
+          // value={geoJson}
+          />
+        </div>
         </div>
     </div>
   );
