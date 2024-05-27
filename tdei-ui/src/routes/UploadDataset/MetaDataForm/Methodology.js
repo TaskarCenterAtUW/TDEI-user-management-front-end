8143d49bb6f50723455a95f3a9538a00a0b0e4b2
shweta2101 <shwethap@vindago.in>
2024-05-27 23:15:50 +0530

    Fixed layout for methodology tab

--- tdei-ui/src/routes/UploadDataset/MetaDataForm/Methodology.js
+++ tdei-ui/src/routes/UploadDataset/MetaDataForm/Methodology.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Formik,Field, ErrorMessage, useFormikContext } from "formik";
+import { Formik, Field, ErrorMessage, useFormikContext } from "formik";
 import { Modal, Button, Form } from "react-bootstrap";
 import Container from "../../../components/Container/Container";
 import Select from 'react-select';
@@ -35,137 +35,125 @@ const Methodology = ({ formData, updateFormData }) => {
   ];
   return (
     <Formik initialValues={formData} onSubmit={values => console.log(values)} >
-    {() => (
-    <div style={{ padding: '5px', marginRight:"20px" }}>
-      <div className="d-flex align-items-center" style={{ marginTop: '20px' }}>
-        <Form.Group className="col-6" controlId="pointDataCollection" style={{ marginRight: '20px' }}>
-          <Form.Label>Point Data Collection</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Point Data Collection Device"
-            name="pointDataCollection"
-          />
-          <ErrorMessage name="pointDataCollection" component="div" />
-        </Form.Group>
-        <Form.Group className="col-6" controlId="nodeSoftware">
-          <Form.Label>Node Locations And Attributes Editing Software</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter Details"
-            name="nodeSoftware"
-          />
-          <ErrorMessage name="nodeSoftware" component="div" />
-        </Form.Group>
-      </div>
-      <div className="d-flex align-items-center" style={{ marginTop: '20px' }}>
-      <Form.Group className="col-4" controlId="dataCollectedByPeople">
-          <Form.Label>Data Collected By People Funded</Form.Label>
-          <RowRadioButtonsGroup
-            radioList={radioList}
-            onRadioSelected={handleDataCollectedByPeople} />
-        </Form.Group>
-        <Form.Group className="col-4" controlId="dataCapturedAutomatically">
-          <Form.Label>Data Captured Automatically</Form.Label>
-          <RowRadioButtonsGroup
-            radioList={radioList}
-            onRadioSelected={handleDataCaptured} />
-        </Form.Group>
-      </div>
-      <div className="d-flex align-items-center" style={{ marginTop: '20px', marginRight:"20px" }}>
-        <Form.Group className="col-4" controlId="dataCollectors" style={{ marginRight: '20px' }}>
-          <Form.Label>Data Collectors</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter Data Collectors"
-            name="dataCollectors"
-          />
-        </Form.Group>
-        <Form.Group className="col-4" controlId="automatedCollection" style={{ marginRight: '20px' }}>
-          <Form.Label>Automated Collection</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter Automated Collection"
-            name="automatedCollection"
-          />
-        </Form.Group>
-        <Form.Group className="col-4" controlId="dataCollectorsOrg">
-          <Form.Label>Data Collectors Organization</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Data Collectors Organization"
-            name="dataCollectorsOrg"
-          />
-        </Form.Group>
-      </div>
-      <div className="d-flex align-items-center" style={{ marginTop: '20px', marginRight:"20px" }}>
-        <Form.Group className="col-4" controlId="dataCollectorCompensation" style={{ marginRight: '20px' }}>
-          <Form.Label>Data Collector Compensation</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Data Collector Compensation"
-            name="dataCollectorCompensation"
-          />
-        </Form.Group>
-        <Form.Group className="col-4" controlId="preprocessingLocation" style={{ marginRight: '20px' }}>
-          <Form.Label>Preprocessing Location</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Preprocessing Location"
-            name="preprocessingLocation"
-          />
-        </Form.Group>
-        <Form.Group className="col-4" controlId="preprocessingBy">
-          <Form.Label>Preprocessing By</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Preprocessing By"
-            name="preprocessingBy"
-          />
-        </Form.Group>
-      </div>
-      <div className="d-flex align-items-center" style={{ marginTop: '20px' }}>
-      <Form.Group className="col-5" controlId="preprocessingDoc" style={{marginRight:"40px" }}>
-          <Form.Label>Data Collection Preprocessing Documentation</Form.Label>
-          <RowRadioButtonsGroup
-            radioList={radioList}
-            onRadioSelected={handlePreprocessingDoc} />
-        </Form.Group>
-        <Form.Group className="col-4" controlId="validProcessExists">
-          <Form.Label>Valid Process Exists</Form.Label>
-          <RowRadioButtonsGroup
-            radioList={radioList}
-            onRadioSelected={handleValidProcessExists} />
-        </Form.Group>
-      </div>
-      <div className="d-flex align-items-center" style={{ marginTop: '20px', marginRight:"20px" }}>
-        <Form.Group className="col-4" controlId="validationConductedBy" style={{ marginRight: '20px' }}>
-          <Form.Label>Validation Conducted By</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Validation Conducted By"
-            name="validationConductedBy"
-          />
-        </Form.Group>
-        <Form.Group className="col-4" controlId="excludedData" style={{ marginRight: '20px' }}>
-          <Form.Label>Excluded Data</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Excluded Data"
-            name="excludedData"
-          />
-        </Form.Group>
-        <Form.Group className="col-4" controlId="excludedDataReason">
-          <Form.Label>Excluded Data Reason</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Excluded Data Reason"
-            name="excludedDataReason"
-          />
-        </Form.Group>
-      </div>
-    </div>
-    )}
-</Formik>
+      {() => (
+        <div className="container">
+          <div className="row" style={{ marginTop: '20px' }}>
+            <div className="col-md-6 column-style">
+              <Form.Group controlId="pointDataCollection">
+                <Form.Label>Point Data Collection</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Point Data Collection Device"
+                  name="pointDataCollection"
+                />
+                <ErrorMessage name="pointDataCollection" component="div" />
+              </Form.Group>
+              <Form.Group controlId="nodeSoftware" style={{ marginTop: '20px' }}>
+                <Form.Label>Node Locations And Attributes Editing Software</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Details"
+                  name="nodeSoftware"
+                />
+                <ErrorMessage name="nodeSoftware" component="div" />
+              </Form.Group>
+              <Form.Group controlId="dataCollectedByPeople" style={{ marginTop: '20px' }}>
+                <Form.Label>Data Collected By People Funded</Form.Label>
+                <RowRadioButtonsGroup
+                  radioList={radioList}
+                  onRadioSelected={handleDataCollectedByPeople} />
+              </Form.Group>
+              <Form.Group controlId="dataCapturedAutomatically" style={{ marginTop: '20px' }}>
+                <Form.Label>Data Captured Automatically</Form.Label>
+                <RowRadioButtonsGroup
+                  radioList={radioList}
+                  onRadioSelected={handleDataCaptured} />
+              </Form.Group>
+
+              <Form.Group controlId="dataCollectors" style={{ marginTop: '20px' }}>
+                <Form.Label>Data Collectors</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Data Collectors"
+                  name="dataCollectors"
+                />
+              </Form.Group>
+              <Form.Group controlId="automatedCollection" style={{ marginTop: '20px' }}>
+                <Form.Label>Automated Collection</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Automated Collection"
+                  name="automatedCollection"
+                />
+              </Form.Group>
+              <Form.Group controlId="dataCollectorsOrg" style={{ marginTop: '20px' }}>
+                <Form.Label>Data Collectors Organization</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Data Collectors Organization"
+                  name="dataCollectorsOrg"
+                />
+              </Form.Group>
+            </div>
+            <div className="col-md-6 column-style">
+              <Form.Group controlId="preprocessingLocation">
+                <Form.Label>Preprocessing Location</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Preprocessing Location"
+                  name="preprocessingLocation"
+                />
+              </Form.Group>
+              <Form.Group controlId="preprocessingBy" style={{ marginTop: '20px' }}>
+                <Form.Label>Preprocessing By</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Preprocessing By"
+                  name="preprocessingBy"
+                />
+              </Form.Group>
+
+              <Form.Group controlId="preprocessingDoc" style={{ marginTop: '20px' }}>
+                <Form.Label>Data Collection Preprocessing Documentation</Form.Label>
+                <RowRadioButtonsGroup
+                  radioList={radioList}
+                  onRadioSelected={handlePreprocessingDoc} />
+              </Form.Group>
+              <Form.Group controlId="validProcessExists" style={{ marginTop: '20px' }}>
+                <Form.Label>Valid Process Exists</Form.Label>
+                <RowRadioButtonsGroup
+                  radioList={radioList}
+                  onRadioSelected={handleValidProcessExists} />
+              </Form.Group>
+              <Form.Group controlId="validationConductedBy" style={{ marginTop: '20px' }}>
+                <Form.Label>Validation Conducted By</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Validation Conducted By"
+                  name="validationConductedBy"
+                />
+              </Form.Group>
+              <Form.Group controlId="excludedData" style={{ marginTop: '20px' }}>
+                <Form.Label>Excluded Data</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Excluded Data"
+                  name="excludedData"
+                />
+              </Form.Group>
+              <Form.Group controlId="excludedDataReason" style={{ marginTop: '20px' }}>
+                <Form.Label>Excluded Data Reason</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Excluded Data Reason"
+                  name="excludedDataReason"
+                />
+              </Form.Group>
+            </div>
+          </div>
+        </div>
+      )}
+    </Formik>
   );
 };
 
