5b3dfc8b71e99dc315e5b6355c10e53ef7f4382d
shweta2101 <shwethap@vindago.in>
2024-05-27 19:44:29 +0530

    layout changes done for datasetsummary tab

--- tdei-ui/src/routes/UploadDataset/MetaDataForm/DatasetSummary.js
+++ tdei-ui/src/routes/UploadDataset/MetaDataForm/DatasetSummary.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik,Field, ErrorMessage, useFormikContext } from "formik";
+import { Formik, Field, ErrorMessage, useFormikContext } from "formik";
 import { Form } from "react-bootstrap";
 import CountriesDropdown from "./DropdownComponents/Countries";
 
@@ -16,72 +16,72 @@ const DatasetSummary = ({ formData, updateFormData }) => {
 
   return (
     <Formik initialValues={formData} onSubmit={values => console.log(values)} >
-    {() => (
-    <div style={{ padding: '5px', marginRight: "20px" }}>
-      <div className="d-flex align-items-center" style={{ marginTop: '20px', marginRight:"20px" }}>
-        <Form.Group className="col-6" controlId="collectionName" style={{ marginRight: '40px' }}>
-          <Form.Label>Collection Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter Collection Name"
-            name="collectionName"
-          />
-        </Form.Group>
-        <Form.Group className="col-6" controlId="departmentName">
-          <Form.Label>Department Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter Department Name"
-            name="departmentName"
-          />
-          <ErrorMessage name="departmentName" component="div" />
-        </Form.Group>
-      </div>
-      <div className="d-flex align-items-center" style={{ marginTop: '10px', marginRight:"20px" }}>
-        <Form.Group className="col-4" controlId="country" style={{ marginRight: '20px' }}>
-          <Form.Label>Country</Form.Label>
-          <Field component={CountriesDropdown} name="country" />
-        </Form.Group>
-        <Form.Group className="col-4" controlId="region" style={{ marginRight: '20px' }}>
-          <Form.Label>Region</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter Region"
-            name="region"
-          />
-          <ErrorMessage name="region" component="div" />
-        </Form.Group>
-        <Form.Group className="col-4" controlId="city" style={{ marginRight: '20px' }}>
-          <Form.Label>City</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter City"
-            name="city"
-          />
-        </Form.Group>
-      </div>
-      <div className="d-flex align-items-center" style={{ marginTop: '20px',  marginRight:"20px" }}>
-        <Form.Group className="col-6" controlId="keyLimitationsOfDataset" style={{ marginRight: '40px' }}>
-          <Form.Label>Key limitations of the dataset</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Key limitations of the dataset"
-            name="keyLimitationsOfDataset"
-          />
-          <ErrorMessage name="keyLimitationsOfDataset" component="div" />
-        </Form.Group>
-        <Form.Group className="col-6" controlId="challenges">
-          <Form.Label>Challenges</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Challenges"
-            name="challenges"
-          />
-        </Form.Group>
-      </div>
-    </div>
-)}
-</Formik>
+      {() => (
+        <div className="container">
+          <div className="row" style={{ marginTop: '20px' }}>
+            <div className="col-md-6 column-style">
+              <Form.Group controlId="collectionName">
+                <Form.Label>Collection Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Collection Name"
+                  name="collectionName"
+                />
+              </Form.Group>
+              <Form.Group controlId="departmentName" style={{ marginTop: '15px' }}>
+                <Form.Label>Department Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Department Name"
+                  name="departmentName"
+                />
+                <ErrorMessage name="departmentName" component="div" />
+              </Form.Group>
+              <Form.Group controlId="keyLimitationsOfDataset" style={{ marginTop: '15px' }}>
+                <Form.Label>Key limitations of the dataset</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Key limitations of the dataset"
+                  name="keyLimitationsOfDataset"
+                />
+                <ErrorMessage name="keyLimitationsOfDataset" component="div" />
+              </Form.Group>
+              <Form.Group controlId="challenges" style={{ marginTop: '15px' }}>
+                <Form.Label>Challenges</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Challenges"
+                  name="challenges"
+                />
+              </Form.Group>
+            </div>
+            <div className="col-md-6 column-style">
+              <Form.Group controlId="city">
+                <Form.Label>City</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter City"
+                  name="city"
+                />
+              </Form.Group>
+              <Form.Group controlId="region" style={{ marginTop: '15px' }}>
+                <Form.Label>Region</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Region"
+                  name="region"
+                />
+                <ErrorMessage name="region" component="div" />
+              </Form.Group>
+              <Form.Group controlId="country" style={{ marginTop: '15px' }}>
+                <Form.Label>Country</Form.Label>
+                <Field component={CountriesDropdown} name="country" />
+              </Form.Group>
+            </div>
+          </div>
+        </div>
+      )}
+    </Formik>
   );
 };
 
