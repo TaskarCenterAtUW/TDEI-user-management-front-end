a1e2af6c9d85723e165432fa9669f8fd73adf194
shweta2101 <shwethap@vindago.in>
2024-05-31 11:23:03 +0530

    Fixed dropdown issue

--- tdei-ui/src/routes/UploadDataset/MetaDataForm/DropdownComponents/DataSource.js
+++ tdei-ui/src/routes/UploadDataset/MetaDataForm/DropdownComponents/DataSource.js
@@ -1,49 +1,43 @@
-import React from 'react';
-import CustomDropdown from '../../../../components/ProjectGroupList/CustomDropdown';
-import InputLabel from '@mui/material/InputLabel';
+import React, { useState, useEffect } from 'react';
 import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import { useState } from 'react';
+import Select from 'react-select';
 
-const DataSourceDropdownForm = ({ field, form, onChange,formDataDataSource }) => {
-  const options = ['TDEITools','3rdParty','InHouse'];
-  const [currentValue,setCurrentValue] = useState(formDataDataSource);
- 
-  const handleChange = (evt) => {
-    const dataSource = evt.target.value;
+const DataSourceDropdownForm = ({ field, form, onChange, formDataDataSource }) => {
+  const options = [
+    { value: 'TDEITools', label: 'TDEI Tools' },
+    { value: '3rdParty', label: 'Third Party' },
+    { value: 'InHouse', label: 'In House' },
+  ];
+  
+  const [currentValue, setCurrentValue] = useState(null);
+
+  useEffect(() => {
+    if (formDataDataSource) {
+      const selectedOption = options.find(option => option.value === formDataDataSource);
+      setCurrentValue(selectedOption);
+    }
+  }, [formDataDataSource]);
+
+  const handleChange = (selectedOption) => {
+    const dataSource = selectedOption ? selectedOption.value : '';
     form.setFieldValue(field.name, dataSource);
     onChange(dataSource);
-    setCurrentValue(dataSource);
+    setCurrentValue(selectedOption);
   };
+
   return (
-    // <CustomDropdown
-    //   placeHolder="Select Source"
-    //   options={options}
-    //   onChange={handleChange}
-    //   field={field}
-    //   form={form}
-    //   defaultValue={formDataDataSource}
-    // />
     <div>
       <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-autowidth-label">Data Source </InputLabel>
         <Select
-          labelId="demo-simple-select-autowidth-label"
-          value={formDataDataSource??currentValue}
+          isSearchable={false}
+          value={currentValue}
           onChange={handleChange}
-          autoWidth
-          label="Select Source"
-        >
-          {options.map((option) => (
-            <MenuItem key={option} value={option} fullWidth>
-              {option}
-            </MenuItem>
-          ))}
-        </Select>
+          options={options}
+          components={{
+            IndicatorSeparator: () => null
+          }} />
       </FormControl>
     </div>
-    
   );
 };
 
