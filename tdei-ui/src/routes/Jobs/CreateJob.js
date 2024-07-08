f36b9913ea37f3e6a3e9ab774e631403cc977053
shweta2101 <shwethap@vindago.in>
2024-07-08 21:51:08 +0530

    added initial Ui configuration based on job type selection

--- tdei-ui/src/routes/Jobs/CreateJob.js
+++ tdei-ui/src/routes/Jobs/CreateJob.js
@@ -8,15 +8,72 @@ import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import useCreateJob from "../../hooks/jobs/useCreateJob";
 import { POST_DATASET } from "../../utils";
-import { Spinner } from "react-bootstrap";
+import { Spinner, Form, Row, Col } from "react-bootstrap";
 import CustomModal from "../../components/SuccessModal/CustomModal";
 import ToastMessage from "../../components/ToastMessage/ToastMessage";
+
+const jobTypeOptions = [
+    { value: 'osw-validate', label: 'OSW - Validate' },
+    { value: 'flex-validate', label: 'Flex - Validate' },
+    { value: 'pathways-validate', label: 'Pathways - Validate' },
+    { value: 'osw-convert', label: 'OSW - Convert' },
+    { value: 'confidence', label: 'Confidence Calculation' },
+    { value: 'quality-metric', label: 'Quality Metric Calculation' },
+    { value: 'dataset-bbox', label: 'Dataset BBox' },
+    { value: 'dataset-tag-road', label: 'Dataset Tag Road' }
+];
+
+const formatOptions = [
+    { value: 'osw', label: 'OSW' },
+    { value: 'osm', label: 'OSM' }
+];
+
+const formConfig = {
+    "osw-convert": [
+        { label: "Source Format", type: "select", options: formatOptions, stateSetter: "setSourceFormat" },
+        { label: "Target Format", type: "select", options: formatOptions, stateSetter: "setTargetFormat" },
+        { label: "Attach data file", type: "dropzone" }
+    ],
+    "flex-validate": [
+        { label: "Attach data file", type: "dropzone" }
+    ],
+    "pathways-validate": [
+        { label: "Attach data file", type: "dropzone" }
+    ],
+    "osw-validate": [
+        { label: "Attach data file", type: "dropzone" }
+    ],
+    "confidence": [
+        { label: "Tdei Dataset Id", type: "text", stateSetter: "setTdeiDatasetId" },
+        { label: "Attach data file", type: "dropzone" }
+    ],
+    "quality-metric": [
+        { label: "Tdei Dataset Id", type: "text", stateSetter: "setTdeiDatasetId" },
+        { label: "Algorithms & Optional Persistence", type: "textarea", stateSetter: "setAlgorithmsJson" }
+    ],
+    "dataset-bbox": [
+        { label: "Tdei Dataset Id", type: "text", stateSetter: "setTdeiDatasetId" },
+        { label: "File Type", type: "select", options: formatOptions, stateSetter: "setFileType" },
+        {
+            label: "Bounding Box Value", type: "bbox", stateSetters: {
+                west: "setBboxWest",
+                south: "setBboxSouth",
+                east: "setBboxEast",
+                north: "setBboxNorth"
+            }
+        }
+    ],
+    "dataset-tag-road": [
+        { label: "Source Dataset Id", type: "text", stateSetter: "setSourceDatasetId" },
+        { label: "Target Dataset Id", type: "text", stateSetter: "setTargetDatasetId" }
+    ]
+};
 
 const CreateJobService = () => {
     const navigate = useNavigate();
     const [jobType, setJobType] = React.useState();
     const [selectedFile, setSelectedFile] = React.useState();
-    const [loading, setLoading] = useState(false); // Track loading state
+    const [loading, setLoading] = useState(false);
     const [showSuccessModal, setShowSuccessModal] = React.useState(false);
     const [errorMessage, setErrorMessage] = useState("");
     const [showToast, setToast] = useState(false);
@@ -24,33 +81,33 @@ const CreateJobService = () => {
     const [targetFormat, setTargetFormat] = React.useState();
     const [showValidateToast, setShowValidateToast] = useState(false);
     const [validateErrorMessage, setValidateErrorMessage] = useState("");
-
-    const jobTypeOptions = [
-        { value: 'osw-validate', label: 'OSW - Validate' },
-        { value: 'flex-validate', label: 'Flex - Validate' },
-        { value: 'pathways-validate', label: 'Pathways - Validate' },
-        { value: 'osw-convert', label: 'OSW - Convert' }
-    ];
-    const formatOptions = [
-        { value: 'osw', label: 'OSW' },
-        { value: 'osm', label: 'OSM' },
-    ];
+    const [tdeiDatasetId, setTdeiDatasetId] = React.useState("");
+    const [sourceDatasetId, setSourceDatasetId] = React.useState("");
+    const [targetDatasetId, setTargetDatasetId] = React.useState("");
+    const [algorithmsJson, setAlgorithmsJson] = React.useState("");
+    const [bboxValues, setBboxValues] = React.useState({
+        west: "",
+        south: "",
+        east: "",
+        north: ""
+    });
+    const [fileType, setFileType] = React.useState();
 
     const onSuccess = (data) => {
         setLoading(false);
-        console.log("sucessfully created", data);
-        // queryClient.invalidateQueries({ queryKey: [POST_DATASET] });
+        console.log("successfully created", data);
         setShowSuccessModal(true);
     };
 
     const onError = (err) => {
         setLoading(false);
-        // resetting current step
         console.error("error message", err);
         setToast(true);
-        setErrorMessage(err.data)
+        setErrorMessage(err.data);
     };
+
     const { isLoading, mutate } = useCreateJob({ onSuccess, onError });
+
     const onStepsComplete = (uploadData) => {
         console.log(uploadData);
         setLoading(true)
@@ -58,13 +115,7 @@ const CreateJobService = () => {
     };
 
     function handleJobTypeSelect(type) {
-        setJobType(type)
-    }
-    function handleSourceFormatSelect(type) {
-        setSourceFormat(type)
-    }
-    function handleTargetFormatSelect(type) {
-        setTargetFormat(type)
+        setJobType(type);
     }
 
     const handlePop = () => {
@@ -73,27 +124,30 @@ const CreateJobService = () => {
 
     const onDrop = (files) => {
         const selectedFile = files[0];
-        setSelectedFile(selectedFile)
+        setSelectedFile(selectedFile);
     };
 
     const handleClose = () => {
         setToast(false);
     };
+
     const handleCloseToast = () => {
         setShowValidateToast(false);
-    }
+    };
+
     const handleCreate = () => {
         if (!jobType) {
             setValidateErrorMessage("Job type is required");
             setShowValidateToast(true);
             return;
         }
-        if (!selectedFile) {
+        if (!selectedFile && ["osw-validate", "flex-validate", "pathways-validate", "osw-convert"].includes(jobType.value)) {
             setValidateErrorMessage("File is required");
             setShowValidateToast(true);
             return;
         }
-        let urlPath = ""
+
+        let urlPath = "";
         switch (jobType?.value) {
             case "osw-validate":
                 urlPath = "osw/validate";
@@ -107,7 +161,20 @@ const CreateJobService = () => {
             case "osw-convert":
                 urlPath = "osw/convert";
                 break;
+            case "confidence":
+                urlPath = "osw/confidence/";
+                break;
+            case "quality-metric":
+                urlPath = "quality-metric/calculate";
+                break;
+            case "dataset-bbox":
+                urlPath = "dataset/bbox";
+                break;
+            case "dataset-tag-road":
+                urlPath = "dataset/tag-road";
+                break;
         }
+
         if (urlPath === "osw/convert") {
             if (!sourceFormat?.value || !targetFormat?.value) {
                 setValidateErrorMessage("Source and target formats are required for OSW - Convert job");
@@ -115,13 +182,134 @@ const CreateJobService = () => {
                 return;
             }
         }
+
         const uploadData = [urlPath, selectedFile];
         if (urlPath === "osw/convert") {
             uploadData.push(sourceFormat.value, targetFormat.value);
         }
-        setLoading(true)
+
+        setLoading(true);
         mutate(uploadData);
-    }
+    };
+
+    const renderFormFields = () => {
+        if (!jobType) return null;
+
+        const fields = formConfig[jobType.value];
+        if (!fields) return null;
+
+        return fields.map((field, index) => {
+            switch (field.type) {
+                case "select":
+                    return (
+                        <div key={index} className={style.formItems}>
+                            <p>{field.label}<span style={{ color: 'red' }}> *</span></p>
+                            <Select
+                                className={style.createJobSelectType}
+                                options={field.options}
+                                placeholder={`Select ${field.label.toLowerCase()}`}
+                                onChange={(value) => {
+                                    if (field.stateSetter === "setSourceFormat") setSourceFormat(value);
+                                    if (field.stateSetter === "setTargetFormat") setTargetFormat(value);
+                                    if (field.stateSetter === "setFileType") setFileType(value);
+                                }}
+                            />
+                        </div>
+                    );
+                case "text":
+                    return (
+                        <Form.Group key={index}>
+                            <Form.Label>{field.label}<span style={{ color: 'red' }}> *</span></Form.Label>
+                            <Form.Control
+                                type="text"
+                                name={field.label}
+                                className={style.createJobSelectType}
+                                onChange={(e) => {
+                                    if (field.stateSetter === "setTdeiDatasetId") setTdeiDatasetId(e.target.value);
+                                    if (field.stateSetter === "setSourceDatasetId") setSourceDatasetId(e.target.value);
+                                    if (field.stateSetter === "setTargetDatasetId") setTargetDatasetId(e.target.value);
+                                }}
+                            />
+                        </Form.Group>
+                    );
+                case "textarea":
+                    return (
+                        <div key={index} style={{ marginTop: '10px' }}>
+                            <Form.Label>{field.label}<span style={{ color: 'red' }}> *</span></Form.Label>
+                            <div className="jsonContent">
+                                <Form.Control
+                                    as="textarea"
+                                    type="text"
+                                    name={field.label}
+                                    onChange={(e) => {
+                                        if (field.stateSetter === "setAlgorithmsJson") setAlgorithmsJson(e.target.value);
+                                    }}
+                                    rows={10}
+                                />
+                            </div>
+                        </div>
+                    );
+                case "dropzone":
+                    return (
+                        <div key={index} className={style.formItems}>
+                            <p>{field.label}<span style={{ color: 'red' }}> *</span></p>
+                            <Dropzone
+                                onDrop={onDrop}
+                                accept={{ 'application/zip': ['.zip'] }}
+                                format=".zip"
+                            />
+                        </div>
+                    );
+                case "bbox":
+                    return (
+                        <div key={index}>
+                            <p>{field.label}<span style={{ color: 'red' }}> *</span></p>
+                            <div>
+                                <Form.Group controlId="west" className={style.bboxFormGroup}>
+                                    <Form.Label className={style.bboxLabel}>West</Form.Label>
+                                    <Form.Control
+                                        className={style.bboxForm}
+                                        type="text"
+                                        placeholder="Enter Coordinates"
+                                        onChange={(e) => setBboxValues({ ...bboxValues, west: e.target.value })}
+                                    />
+
+                                </Form.Group>
+                                <Form.Group controlId="south" className={style.bboxFormGroup}>
+                                    <Form.Label className={style.bboxLabel}>South</Form.Label>
+                                    <Form.Control
+                                        className={style.bboxForm}
+                                        type="text"
+                                        placeholder="Enter Coordinates"
+                                        onChange={(e) => setBboxValues({ ...bboxValues, south: e.target.value })}
+                                    />
+                                </Form.Group>
+                                <Form.Group controlId="east" className={style.bboxFormGroup}>
+                                    <Form.Label className={style.bboxLabel}>East</Form.Label>
+                                    <Form.Control
+                                        className={style.bboxForm}
+                                        type="text"
+                                        placeholder="Enter Coordinates"
+                                        onChange={(e) => setBboxValues({ ...bboxValues, east: e.target.value })}
+                                    />
+                                </Form.Group>
+                                <Form.Group controlId="north" className={style.bboxFormGroup}>
+                                    <Form.Label className={style.bboxLabel}>North</Form.Label>
+                                    <Form.Control
+                                        className={style.bboxForm}
+                                        type="text"
+                                        placeholder="Enter Coordinates"
+                                        onChange={(e) => setBboxValues({ ...bboxValues, north: e.target.value })}
+                                    />
+                                </Form.Group>
+                            </div>
+                        </div>
+                    );
+                default:
+                    return null;
+            }
+        });
+    };
 
     return (
         <Layout>
@@ -133,47 +321,30 @@ const CreateJobService = () => {
                         <form className={style.form}>
                             <div className={style.formItems}>
                                 <p>Job Type<span style={{ color: 'red' }}> *</span></p>
-                                <Select className={style.createJobSelectType}
+                                <Select
+                                    className={style.createJobSelectType}
                                     options={jobTypeOptions}
                                     placeholder="Select a Job type"
                                     onChange={handleJobTypeSelect}
                                 />
                             </div>
-                            { jobType && jobType.value && jobType.value === "osw-convert" && (
-                                <div>
-                                    <div className={style.formItems}>
-                                        <p>Source Format<span style={{ color: 'red' }}> *</span></p>
-                                        <Select className={style.createJobSelectType}
-                                            options={formatOptions}
-                                            placeholder="Select source format"
-                                            onChange={handleSourceFormatSelect}
-                                        />
-                                    </div>
-                                    <div className={style.formItems}>
-                                        <p>Target Format<span style={{ color: 'red' }}> *</span></p>
-                                        <Select className={style.createJobSelectType}
-                                            options={formatOptions}
-                                            placeholder="Select target format"
-                                            onChange={handleTargetFormatSelect}
-                                        />
-                                    </div>
-                                </div>
-
-                            )}
-                            <div className={style.formItems}>
-                                <p>Attach data file<span style={{ color: 'red' }}> *</span></p>
-                                <Dropzone onDrop={onDrop} accept={{
-                                    'application/zip': ['.zip']
-                                }} format={".zip"} />
-                            </div>
+                            {renderFormFields()}
                         </form>
                     </div>
                     <div className={style.divider}></div>
                     <div className={style.buttonContainer}>
-                        <Button className={style.buttonSecondaryCustomised}
-                            onClick={handlePop}>Cancel</Button>
-                        <Button className={`tdei-primary-button ${style.textUnset}`}
-                            onClick={handleCreate}>Create</Button>
+                        <Button
+                            className={style.buttonSecondaryCustomised}
+                            onClick={handlePop}
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            className={`tdei-primary-button ${style.textUnset}`}
+                            onClick={handleCreate}
+                        >
+                            Create
+                        </Button>
                     </div>
 
                     {loading && (
@@ -209,7 +380,12 @@ const CreateJobService = () => {
                             title="Error"
                         />
                     )}
-                     <ToastMessage showToast={showValidateToast} toastMessage={validateErrorMessage} onClose={handleCloseToast} isSuccess={false} />
+                    <ToastMessage
+                        showToast={showValidateToast}
+                        toastMessage={validateErrorMessage}
+                        onClose={handleCloseToast}
+                        isSuccess={false}
+                    />
                 </>
             </div>
         </Layout>
