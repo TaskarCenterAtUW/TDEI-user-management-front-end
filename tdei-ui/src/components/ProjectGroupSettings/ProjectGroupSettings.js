afb453d2bdd19c70235825a4f318714355391fc0
SrikanthV <srikanthv@vindago.in>
2025-08-25 12:48:32 +0530

    Updated Dataviewer base URL

--- tdei-ui/src/components/ProjectGroupSettings/ProjectGroupSettings.js
+++ tdei-ui/src/components/ProjectGroupSettings/ProjectGroupSettings.js
@@ -55,7 +55,7 @@ const ProjectGroupSettings = (props) => {
   });
 
   const generateViewerUrl = () => {
-    return `https://dataviewer-dev.tdei.us/${projectGroup.tdei_project_group_id}`;
+    return `https://dataviewer-dev.tdei.us/project-group/${projectGroup.tdei_project_group_id}`;
   };
 
   const copyViewerUrl = async () => {
